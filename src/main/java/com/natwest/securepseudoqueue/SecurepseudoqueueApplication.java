752dc3490ea925f2857163bd538827f2f7616738
RANAJITPAL <palranajit98@gmail.com>
2022-04-09 09:57:27 +0530

    Initial commit

---
+++ src/main/java/com/natwest/securepseudoqueue/SecurepseudoqueueApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.securepseudoqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepseudoqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepseudoqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/securepseudoqueue/SecurepseudoqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.securepseudoqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepseudoqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
